9c0cce8c3aea49ca98774f07047ad96f014a05bf
Joachim Kret <joachimkret@macbook.pro>
2021-08-16 20:03:06 +0200

    Added Operations project.

---
+++ Sources/Operations.h
@@ -0,0 +1,18 @@
+//
+//  Operations.h
+//  Operations
+//
+//  Created by Joachim Kret on 16/08/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Operations.
+FOUNDATION_EXPORT double OperationsVersionNumber;
+
+//! Project version string for Operations.
+FOUNDATION_EXPORT const unsigned char OperationsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Operations/PublicHeader.h>
+
+
